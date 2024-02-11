30544b4310c6645cc2dd980f5e47aabdc59077a8
Daamiiaan <127410766+Daamiiaan@users.noreply.github.com>
2024-02-11 09:21:52 +0100

    Update z2.cpp

--- z2.cpp
+++ z2.cpp
@@ -1,25 +1,40 @@
-int main(){
-    const punkt* wsk1=new punkt(1.1,2.2);
-    const wierzcholek* wsk2= new wierzcholek(3.3,4.4,"red");
-
-    wierzcholek ob1=wierzcholek(7,7,"green");
-    cout<< ob1[0]<<" "<<ob1[1]<<endl;
-
-    punkt ob2= *wsk1; delete wsk1; wsk1=0;
-    wierzcholek ob3 = *wsk2; delete wsk2; wsk2=0;
-
-    punkt* tab1[4];
-
-    tab1[0]=new wierzcholek(ob1);
-    tab1[1]=new punkt(ob2);
-    tab1[2]=new wierzcholek(ob3);
-    tab1[3]=new wierzcholek;
-
-    for (int i=0;i<4;++i)
-    {
-        cout<<*tab1[i];
-        delete tab1[i];
-
-    }
-    cout<<"****3****"<<endl;
-return 0;
+class punkt{
+public:
+double* dane;
+punkt():dane(new double[2]{0,0}){}
+punkt(const double d1,const double d2):dane(new double[2]){
+    dane[0]=d1;
+    dane[1]=d2;}
+punkt(const punkt& pe):dane(new double[2]){
+    dane[0]=pe.dane[0];
+    dane[1]=pe.dane[1];}
+virtual ~punkt(){delete []dane;}
+virtual ostream& show(ostream& out) const{
+    return out<<"( "<<dane[0]<<" "<<dane[1]<<" )"<<endl;};
+punkt& operator=(const punkt& pu){
+    if(this!=&pu){
+        dane[0]=pu.dane[0];
+        dane[1]=pu.dane[1];}
+    return *this;}};
+ostream& operator<<(ostream& out, const punkt& pi){
+    return pi.show(out);}
+class wierzcholek:public punkt{
+protected:
+string kolor;
+public:
+wierzcholek():punkt(),kolor("brak"){}
+wierzcholek(const double p1,const double p2, const string& x):punkt(p1,p2),kolor(x){}
+wierzcholek(const wierzcholek& wi):punkt(wi.dane[0],wi.dane[1]),kolor(wi.kolor){}
+~wierzcholek(){}
+ostream& show(ostream& out) const override{
+    return out<<"( "<<dane[0]<<" "<<dane[1]<<" - "<<kolor<<" )"<<endl;}
+wierzcholek& operator=(const wierzcholek& wietnam){
+    if(this!=&wietnam){
+        dane[0]=wietnam.dane[0];
+        dane[1]=wietnam.dane[1];
+        kolor=wietnam.kolor;}
+    return *this;}
+double operator[](const int pos){
+    if(pos>1){
+        throw string("Indeks poza zakresem");}
+    return dane[pos];}};
