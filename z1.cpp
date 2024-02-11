19d9dbe2ef41191604dc9018e1766c25704642c2
Daamiiaan <127410766+Daamiiaan@users.noreply.github.com>
2024-02-11 10:46:04 +0100

    Update z1.cpp

--- z1.cpp
+++ z1.cpp
@@ -1,31 +1,54 @@
-#include <iostream>
-using namespace std;
-
 class K1
 {
-string* p1;
+string *P1;
+
 public:
-K1() : p1(new string("brak")){}
-K1(string wartosc) : p1(new string(wartosc)) {}
-friend ostream operator<< (ostream& wy, const K1 c);
-};
-ostream operator<< (ostream& wy, const K1& c)
+
+K1():P1(new string("brak")){}
+K1(string a):P1(new string(a)){}
+K1(const K1 &a)
 {
-  wy << *c.p1 << endl;
-  return wy;
+P1=new string();
+*P1=*a.P1;
+}
+~K1()
+{
+delete P1;
+}
+
+K1 &operator=(K1 c)
+{
+*P1=*c.P1;
+return *this;
+}
+
+friend ostream &operator<< (ostream &wypisz,const K1 a);
+};
+
+ostream &operator<<(ostream &wypisz,const K1 a)
+{
+wypisz<<*a.P1< return wypisz;
 }
 
 class K2
 {
-  K1 tab[2];
-  int w1;
-  public:
-  K2() : tab{K1(), K1()}, w1(0){}
-  K2(string a, string b, int c) : tab{K1(a), K2(b)}, w1(c) {}
-  friend ostream & operator<< (ostream& wy, const K2& c);
-};
-ostream & operator<<(ostream& out, const K2& wy)
+K1 tab[2];
+int w1;
+
+public:
+
+K2():tab{K1(),K1()},w1(0){}
+K2(string a,string b, int c):tab{K1(a),K1(b)},w1(c){}
+
+K2 operator+(int a)
 {
-  out << wy.tab[0] << wy.tab[1] << wy.w1 << endl;
-  return out;
+this->w1=w1+a;
+return *this;
+}
+friend ostream &operator<<(ostream &wypisz, const K2 b);
+};
+
+ostream &operator<<(ostream &wypisz, const K2 b)
+{
+wypisz< return wypisz;
 }
